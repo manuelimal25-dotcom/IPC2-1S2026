baec7f172f1fb50c93629b52349fb5088f55cb04
manuelimal02 <manuelimal02@gmail.com>
2026-03-17 16:49:34 -0600

    Semana 8

--- Semana_8/Controllers/DronController.cs
+++ Semana_8/Controllers/DronController.cs
@@ -1,13 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using Semana_8.Models;
 
 namespace Semana_8.Controllers
 {
     public class DronController : Controller
     {
+        // Lista estática para mantener los drones entre peticiones
+        private static ListaDrones listaDrones = new ListaDrones();
+
         // GET: /Dron/Index
         public IActionResult Index()
         {
-            return View();
+            return View(listaDrones);
+        }
+
+        // POST: /Dron/Agregar
+        [HttpPost]
+        public IActionResult Agregar(string nombreDron)
+        {
+            if (!string.IsNullOrWhiteSpace(nombreDron))
+            {
+                Dron nuevoDron = new Dron(nombreDron);
+                listaDrones.InsertarDron(nuevoDron);
+                Console.WriteLine($"Dron '{nombreDron}' agregado correctamente.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: /Dron/MostrarLista
+        [HttpPost]
+        public IActionResult MostrarLista()
+        {
+            return View("Index", listaDrones);
         }
     }
 }
